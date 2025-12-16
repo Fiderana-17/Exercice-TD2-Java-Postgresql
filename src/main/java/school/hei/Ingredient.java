0d20cc6c7d575a8db45b23ac8b0b200b3e72d9b8
Fiderana-17 <hei.fiderana@gmail.com>
2025-12-16 14:58:51 +0300

    chore: Ingredient init

---
+++ src/main/java/school/hei/Ingredient.java
@@ -0,0 +1,21 @@
+package school.hei;
+
+public class Ingredient {
+    private int id;
+    private String name;
+    private double price;
+    private CategoryEnum category;
+    private Dish dish;
+
+    public Ingredient(int id, String name, double price, CategoryEnum category, Dish dish) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.category = category;
+        this.dish = dish;
+    }
+
+    public String getDishName() {
+        return dish == null ? null : dish.getName();
+    }
+}
