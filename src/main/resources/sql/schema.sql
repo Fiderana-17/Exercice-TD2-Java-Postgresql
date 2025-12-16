d129e14f1e0b5b1997aaeee78ba78dddb3c3a360
Fiderana-17 <hei.fiderana@gmail.com>
2025-12-16 14:37:59 +0300

    chore: init schema

--- src/main/resources/sql/schema.sql
+++ src/main/resources/sql/schema.sql
@@ -1,8 +1,8 @@
 CREATE TYPE category_enum AS ENUM ('VEGETABLE', 'ANIMAL', 'MARINE', 'DAIRY', 'OTHER');
 CREATE TYPE dish_type_enum AS ENUM ('START', 'MAIN', 'DESSERT');
 
-Create table dish(
-    id serial not null,
+Create table Dish(
+    id serial primary key ,
     name varchar(255) not null,
     dish_type dish_type_enum not null
 );
@@ -12,5 +12,6 @@ Create table Ingredient(
     name varchar(255) not null,
     price numeric not null,
     category category_enum not null,
-    id_dish Integer references dish(id)
+    id_dish INT,
+    CONSTRAINT fk_dish FOREIGN KEY (id_dish) REFERENCES Dish(id) ON DELETE SET NULL
 )
